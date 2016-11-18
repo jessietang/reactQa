45e413296823649280b076a8563587f9adfc39e9
jessietang <jessietang@aaxischina.com>
2016-11-18 16:09:06 +0800

    add some notes

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var QuestionApp = require('./components/QuestionApp.js');
+/*模块名可使用相对路径（以./开头），或者是绝对路径（以/或C:之类的盘符开头）*/
 
 var mainCom = ReactDOM.render(
     <QuestionApp />,
